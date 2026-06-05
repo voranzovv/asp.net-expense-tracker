6a5027a18a5a50ab74a99ffb8d1f7fea3a57f1db
voranzov <voranzov@gmail.com>
2026-06-05 07:10:57 -0400

    2nd  commit

---
+++ Expense-tracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_tracker.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
